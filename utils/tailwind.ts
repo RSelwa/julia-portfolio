e87457d917cae1851396d1ae16def68a17bdfe6b
selwa <selwa.raphael@gmail.com>
2024-10-24 17:20:49 +0200

    🎨 Fix: fix prettier for build

--- utils/tailwind.ts
+++ utils/tailwind.ts
@@ -1,4 +1,4 @@
-import cx, { type ArgumentArray } from 'classnames'
-import { twMerge } from 'tailwind-merge'
+import cx, { type ArgumentArray } from "classnames"
+import { twMerge } from "tailwind-merge"
 
 export const cn = (...classes: ArgumentArray) => twMerge(cx(classes))
